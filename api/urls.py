084565cbf3a7f605507d331b37ee5a26e8e6294e
Amine <149009322+steakmeatdev@users.noreply.github.com>
2025-01-13 14:31:08 +0100

    Model and serializer

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 urlpatterns = [
-    path("", main)
+    path("home", RoomView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
-def main(request):
-    return HttpResponse("Hello Algeria")
+
+# set up a generic view with generics.ListAPIView to return all the rooms as JSON
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
