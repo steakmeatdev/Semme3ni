defec8bda4920b67baaf3dfd046e4b95ba38ac0a
Amine <149009322+steakmeatdev@users.noreply.github.com>
2025-01-15 01:19:55 +0100

    handling room creations

--- api/views.py
+++ api/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-# set up a generic view with generics.ListAPIView to return all the rooms as JSON
+# Set up a generic view with generics.ListAPIView to display all the Room objects
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request):
+        # Check active session
+        if not self.request.session.session_key:
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.validated_data.get("guest_can_pause")
+            votes_to_skip = serializer.validated_data.get("votes_to_skip")
+            host = self.request.session.session_key
+
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=["guest_can_pause", "votes_to_skip"])
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+
+            return Response(CreateRoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
